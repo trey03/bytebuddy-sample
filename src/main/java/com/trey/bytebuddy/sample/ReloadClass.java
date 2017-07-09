8f50333a931a9fc9878fc9122e8fe889f8a2ba72
c feng <c feng>
2017-07-09 17:56:03 +0800

    pom bytebuddy-agent:1.7.1;perfect the reload class sample

--- src/main/java/com/trey/bytebuddy/sample/ReloadClass.java
+++ src/main/java/com/trey/bytebuddy/sample/ReloadClass.java
@@ -2,28 +2,42 @@ package com.trey.bytebuddy.sample;
 
 import net.bytebuddy.ByteBuddy;
 import net.bytebuddy.agent.ByteBuddyAgent;
+import net.bytebuddy.dynamic.DynamicType;
 import net.bytebuddy.dynamic.loading.ClassReloadingStrategy;
 
 /**
  * Created by chenfeng on 2017/7/8.
  */
 public class ReloadClass {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Throwable {
         ByteBuddyAgent.install();
+
         Foo foo = new Foo();
-        new ByteBuddy()
+        System.out.println(foo.m());
+        System.out.println(foo.m("test"));
+        System.out.println(foo.m1());
+
+        DynamicType.Loaded loaded = new ByteBuddy()
                 .redefine(Bar.class)
                 .name(Foo.class.getName())
                 .make()
                 .load(Foo.class.getClassLoader(), ClassReloadingStrategy.fromInstalledAgent());
 
+        loaded.saveIn(SampleHelper.INSTACE.getSaveDir());
+
         System.out.println(foo.m());
+        System.out.println(foo.m("test"));
+        System.out.println(foo.m1());
     }
 }
 class Foo {
-    String m() { return "foo"; }
+    String m() { return "foo."; }
+    String m(String a) {return "foo "+a ; }
+    String m1() {return "foo m1."; }
 }
 
 class Bar {
-    String m() { return "bar"; }
+    String m() { return "bar."; }
+    String m(String a) {return "bar "+a ; }
+    String m1() {return "bar m1."; }
 }
